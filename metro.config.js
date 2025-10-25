d0100984adc0b78315d64e3f123fadcf99f810f9
SumanthCsy <sumanthcsy04@gmail.com>
2025-10-25 18:33:53 +0530

    Update metro.config.js with module resolution fixes

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,14 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const config = getDefaultConfig(__dirname);
 
-module.exports = getDefaultConfig(__dirname);
+// Add this to resolve the duplicate module issue
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  // Add any specific path resolutions if needed
+};
+
+// Add this to ignore problematic node modules
+config.resolver.blacklistRE = /#(.*)/;
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
+
+module.exports = config;
